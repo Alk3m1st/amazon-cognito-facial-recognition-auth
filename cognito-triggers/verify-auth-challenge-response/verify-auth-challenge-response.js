81e9a79bdb14dc56d2a13b31d06ef9b6e20d5a94
Allan Oliveira <a.oliveira@curtin.edu.au>
2024-05-07 15:32:09 +0800

    fix: Update AWS SDK to v3 and provide sign in S3 bucket env var
    Verify Auth Challenge Lambda

--- cognito-triggers/verify-auth-challenge-response/verify-auth-challenge-response.js
+++ cognito-triggers/verify-auth-challenge-response/verify-auth-challenge-response.js
@@ -1,45 +1,52 @@
-var aws = require('aws-sdk');
-var rekognition = new aws.Rekognition();
+const {
+  RekognitionClient,
+  SearchFacesByImageCommand,
+} = require("@aws-sdk/client-rekognition");
+
+const rekognitionClient = new RekognitionClient();
 
 exports.handler = async (event, context) => {
+  console.log("Verify Auth Challenge: " + JSON.stringify(event));
+  let userPhoto = "";
+  event.response.answerCorrect = false;
 
-    console.log("Verify Auth Challenge: " + JSON.stringify(event));
-    let userPhoto = '';
-    event.response.answerCorrect = false;
+  // Searching existing faces indexed on Rekognition using the provided photo on s3
 
-    // Searching existing faces indexed on Rekognition using the provided photo on s3
+  const objectName = event.request.challengeAnswer;
+  const searchCmd = new SearchFacesByImageCommand({
+    CollectionId: process.env.COLLECTION_NAME,
+    Image: {
+      S3Object: {
+        Bucket: process.env.BUCKET_SIGN_IN,
+        Name: objectName,
+      },
+    },
+    MaxFaces: 1,
+    FaceMatchThreshold: 90,
+    // QualityFilter: "AUTO",
+  });
+  try {
+    const { FaceMatches } = await rekognitionClient.send(searchCmd);
 
-    const objectName = event.request.challengeAnswer;
-    const params = {
-        "CollectionId": process.env.COLLECTION_NAME,
-        "Image": {
-            "S3Object": {
-                "Bucket": process.env.BUCKET_SIGN_UP,
-                "Name": objectName
-            }
-        },
-        "MaxFaces": 1,
-        "FaceMatchThreshold": 90
-    };
-    try {
-        const data = await rekognition.searchFacesByImage(params).promise();
+    // Evaluates if Rekognition was able to find a match with the required
+    // confidence threshold
 
-        // Evaluates if Rekognition was able to find a match with the required 
-        // confidence threshold
-
-        if (data.FaceMatches[0]) {
-            console.log('Face Id: ' + data.FaceMatches[0].Face.FaceId);
-            console.log('Similarity: ' + data.FaceMatches[0].Similarity);
-            userPhoto = data.FaceMatches[0].Face.FaceId;
-            if (userPhoto) {
-                if (event.request.privateChallengeParameters.answer == userPhoto) {
-                    event.response.answerCorrect = true;
-                }
-            }
+    if (FaceMatches && FaceMatches[0]) {
+      console.log("Face Id: " + FaceMatches[0].Face.FaceId);
+      console.log("Similarity: " + FaceMatches[0].Similarity);
+      userPhoto = FaceMatches[0].Face.FaceId;
+      if (userPhoto) {
+        if (event.request.privateChallengeParameters.answer == userPhoto) {
+          event.response.answerCorrect = true;
         }
-    } catch (err) {
-        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-        throw err;
+      }
     }
-    return event;
-}
+  } catch (err) {
+    console.error(
+      "Unable to query rekognition. Error:",
+      JSON.stringify(err, null, 2)
+    );
+    throw err;
+  }
+  return event;
+};
